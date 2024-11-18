6ce5f3d09568b82629e5d9f8ad6ce687e006f848
Weikai Rao <weikairao@gmail.com>
2024-11-18 01:13:21 -0600

    Update admin.py

--- dbconnect/admin.py
+++ dbconnect/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import StlDistrict, StlDisciplineIncidents, StlDemographic, StlActScore, StlStudentStaffRatio
+
+# Register each model
+admin.site.register(StlDistrict)
+admin.site.register(StlDisciplineIncidents)
+admin.site.register(StlDemographic)
+admin.site.register(StlActScore)
+admin.site.register(StlStudentStaffRatio)
